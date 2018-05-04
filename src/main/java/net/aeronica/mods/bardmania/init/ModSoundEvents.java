edebb2c7d83b2bdc197b8a0e5f91e43f4c348c8b
Aeronica <pmboese@gmail.com>
2018-05-03 23:33:46 -0500

    Allow specifying MOD and/or Vanilla sounds in the instruments.json file.

--- src/main/java/net/aeronica/mods/bardmania/init/ModSoundEvents.java
+++ src/main/java/net/aeronica/mods/bardmania/init/ModSoundEvents.java
@@ -3,6 +3,9 @@ package net.aeronica.mods.bardmania.init;
 import net.aeronica.mods.bardmania.Reference;
 import net.aeronica.mods.bardmania.item.ItemHandHeld;
 import net.aeronica.mods.bardmania.object.Instrument;
+import net.minecraft.client.resources.IResourceManager;
+import net.minecraft.client.resources.IResourceManagerReloadListener;
+import net.minecraft.client.resources.ResourcePackRepository;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvent;
 import net.minecraftforge.event.RegistryEvent;
@@ -19,27 +22,31 @@ public class ModSoundEvents {
 
     private static final Map<String, SoundEvent> SOUNDS = new HashMap<>();
 
-    static
-    {
-        for(ItemHandHeld handHeld : ModInstruments.INSTRUMENTS)
-        {
+    static {
+        for (ItemHandHeld handHeld : ModInstruments.INSTRUMENTS) {
             Instrument instrument = handHeld.getInstrument();
-            if(!SOUNDS.containsKey(instrument.sounds.timbre))
-            {
+            if (!SOUNDS.containsKey(instrument.sounds.timbre)) {
                 SOUNDS.put(instrument.sounds.timbre, registerSound(instrument.sounds.timbre));
             }
         }
     }
+
     /**
      * Register a {@link SoundEvent}.
      *
-     * @param soundName The SoundEvent's name without the [MOD_ID] prefix
+     * @param soundName The SoundEvent's name with or without the [MOD_ID] prefix. With prefix uses the mods sound
+     *                  resource. Without the prefix it will attempt to use the vanilla sound resource.
      * @return The SoundEvent
      * @author Choonster
      */
+    @Nullable
     private static SoundEvent registerSound(String soundName) {
-        ResourceLocation soundID = new ResourceLocation(Reference.MOD_ID, soundName);
-        SoundEvent soundEvent = new SoundEvent(soundID).setRegistryName(soundID);
+        final ResourceLocation soundID = new ResourceLocation(soundName);
+        SoundEvent soundEvent;
+        if (soundName.contains(":"))
+            soundEvent = new SoundEvent(soundID).setRegistryName(soundID);
+        else
+            soundEvent = SoundEvent.REGISTRY.getObject(soundID);
         // TODO: For this mod see if the soundName is already registered. Hmmm not sure other than MC sounds it that's possble.
         // Anyway the sound name from the json file needs to allow specifying a sound resource from Vanilla or this mod.
         // Probably can't use another mods sounds at this stage, but it should be possible to use Vanilla sounds
