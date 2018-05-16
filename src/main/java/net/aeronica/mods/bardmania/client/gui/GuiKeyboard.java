aade7f377e5937b693c66f86c2a0b38043105374
Aeronica <pmboese@gmail.com>
2018-05-15 21:55:33 -0500

    Minor cleanup

--- src/main/java/net/aeronica/mods/bardmania/client/gui/GuiKeyboard.java
+++ src/main/java/net/aeronica/mods/bardmania/client/gui/GuiKeyboard.java
@@ -3,33 +3,39 @@ package net.aeronica.mods.bardmania.client.gui;
 import net.aeronica.mods.bardmania.network.PacketDispatcher;
 import net.aeronica.mods.bardmania.network.server.ActiveReceiverMessage;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
-import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.client.resources.I18n;
 import org.lwjgl.input.Keyboard;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class GuiKeyboard extends GuiScreen
 {
-    private static final Integer[][] KEY_VALUES = new Integer[][]{
+    String TITLE = I18n.format("gui.bardmania.gui_keyboard.title");
+    private static final Integer[][] KEYNOTE_VALUES = new Integer[][]{
             {Keyboard.KEY_Q, 48}, {Keyboard.KEY_2, 49}, {Keyboard.KEY_W, 50}, {Keyboard.KEY_3, 51},
             {Keyboard.KEY_E, 52}, {Keyboard.KEY_R, 53}, {Keyboard.KEY_5, 54}, {Keyboard.KEY_T, 55},
             {Keyboard.KEY_6, 56}, {Keyboard.KEY_Y, 57}, {Keyboard.KEY_7, 58}, {Keyboard.KEY_U, 59},
             {Keyboard.KEY_I, 60}};
-    private static final Map<Integer, Integer> keyMap;
-    static {
+    private static final Map<Integer, Integer> keyNoteMap;
+
+    static
+    {
         Map<Integer, Integer> aMap = new HashMap<>();
-        for (Integer[] key : KEY_VALUES)
+        for (Integer[] key : KEYNOTE_VALUES)
         {
             aMap.put(key[0], key[1]);
         }
-        keyMap = Collections.unmodifiableMap(aMap);
+        keyNoteMap = Collections.unmodifiableMap(aMap);
     }
 
-    public GuiKeyboard ()
+    public GuiKeyboard()
     {
     }
 
@@ -37,6 +43,12 @@ public class GuiKeyboard extends GuiScreen
     public void initGui()
     {
         super.initGui();
+    }
+
+    @Override
+    public void onGuiClosed()
+    {
+        super.onGuiClosed();
     }
 
     @Override
@@ -49,10 +61,9 @@ public class GuiKeyboard extends GuiScreen
     public void drawScreen(int mouseX, int mouseY, float partialTicks)
     {
         drawDefaultBackground();
-        String localTITLE = "Gui Keyboard";
-        int posX = (this.width - getFontRenderer().getStringWidth(localTITLE)) / 2 ;
+        int posX = (this.width - getFontRenderer().getStringWidth(TITLE)) / 2;
         int posY = 10;
-        getFontRenderer().drawStringWithShadow(localTITLE, posX, posY, 0xD3D3D3);
+        getFontRenderer().drawStringWithShadow(TITLE, posX, posY, 0xD3D3D3);
         super.drawScreen(mouseX, mouseY, partialTicks);
     }
 
@@ -65,8 +76,8 @@ public class GuiKeyboard extends GuiScreen
     @Override
     protected void keyTyped(char typedChar, int keyCode) throws IOException
     {
-        if (keyMap.containsKey(keyCode))
-            sendNote(keyMap.get(keyCode) + (isShiftKeyDown() ? 12: 0));
+        if (keyNoteMap.containsKey(keyCode) && !Keyboard.isRepeatEvent())
+            sendNote(keyNoteMap.get(keyCode) + (isShiftKeyDown() ? 12 : 0));
         super.keyTyped(typedChar, keyCode);
     }
 
@@ -84,11 +95,13 @@ public class GuiKeyboard extends GuiScreen
 
     private void sendNote(int note)
     {
-        EntityPlayer player = mc.player;
-        // NOTE_ON | NOTE_OFF MIDI message [ (message & 0xF0 | channel & 0x0F), note, volume ]
-        ActiveReceiverMessage packet = new ActiveReceiverMessage(player.getPosition(), player.getEntityId(), player.getActiveHand(), (byte)note, (byte)127);
+        EntityPlayerSP player = mc.player;
+        ActiveReceiverMessage packet = new ActiveReceiverMessage(player.getPosition(), player.getEntityId(), player.getActiveHand(), (byte) note, (byte) 127);
         PacketDispatcher.sendToServer(packet);
     }
 
-    private FontRenderer getFontRenderer() {return mc.fontRenderer;}
+    private FontRenderer getFontRenderer()
+    {
+        return mc.fontRenderer;
+    }
 }
