a3d3826a151fe5a45a96002112f36b670e12a5e9
Aeronica <pmboese@gmail.com>
2018-05-22 23:51:26 -0500

    First pass at adding keyboard buttons to the gui

---
+++ src/main/java/net/aeronica/mods/bardmania/client/gui/GuiKeyButton.java
@@ -0,0 +1,57 @@
+package net.aeronica.mods.bardmania.client.gui;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.audio.SoundHandler;
+import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.renderer.GlStateManager;
+
+public class GuiKeyButton extends GuiButton
+{
+    public GuiKeyButton(int buttonId, int x, int y, int widthIn, int heightIn, String buttonText)
+    {
+        super(buttonId, x, y, widthIn, heightIn, buttonText);
+    }
+
+    @Override
+    public void drawButton(Minecraft mc, int mouseX, int mouseY, float partialTicks)
+    {
+        if (this.visible)
+        {
+            FontRenderer fontrenderer = mc.fontRenderer;
+            GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+            this.hovered = mouseX >= this.x && mouseY >= this.y && mouseX < this.x + this.width && mouseY < this.y + this.height;
+            int i = this.getHoverState(this.hovered);
+            GlStateManager.enableBlend();
+            GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
+            GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
+            this.drawBox(this.x,this.y,this.width / 2, this.height);
+            this.drawBox(this.x + this.width / 2, this.y,this.width / 2, this.height);
+            this.mouseDragged(mc, mouseX, mouseY);
+            int j = 14737632;
+
+            if (packedFGColour != 0)
+            {
+                j = packedFGColour;
+            }
+            else
+            if (!this.enabled)
+            {
+                j = 10526880;
+            }
+            else if (this.hovered)
+            {
+                j = 16777120;
+            }
+            this.drawCenteredString(fontrenderer, this.displayString, this.x + this.width / 2, this.y + (this.height - 8) / 2, j);
+        }
+    }
+
+    @Override
+    public void playPressSound(SoundHandler soundHandlerIn) {/* NOP */}
+
+    public void drawBox(int x, int y, int width, int height) {
+        drawRect(x, y, x + width, y + height, 0x55000000);
+        drawRect(x - 2, y - 2, x + width + 2, y + height + 2, 0x44000000);
+    }
+}
--- src/main/java/net/aeronica/mods/bardmania/client/gui/GuiKeyboard.java
+++ src/main/java/net/aeronica/mods/bardmania/client/gui/GuiKeyboard.java
@@ -12,6 +12,20 @@ import java.io.IOException;
 
 public class GuiKeyboard extends GuiScreen
 {
+    private static final Integer[] KEYSCAN_VALUES = new Integer[]{
+            Keyboard.KEY_Z, Keyboard.KEY_S, Keyboard.KEY_X, Keyboard.KEY_D,
+            Keyboard.KEY_C, Keyboard.KEY_V, Keyboard.KEY_G, Keyboard.KEY_B,
+            Keyboard.KEY_H, Keyboard.KEY_N, Keyboard.KEY_J, Keyboard.KEY_M,
+            Keyboard.KEY_COMMA,
+            Keyboard.KEY_Q, Keyboard.KEY_2, Keyboard.KEY_W, Keyboard.KEY_3,
+            Keyboard.KEY_E, Keyboard.KEY_R, Keyboard.KEY_5, Keyboard.KEY_T,
+            Keyboard.KEY_6, Keyboard.KEY_Y, Keyboard.KEY_7, Keyboard.KEY_U,
+            Keyboard.KEY_I};
+    private static final String KEY_TOPS = "ZSXDCVGBHNJM,Q2W3ER5T6Y7UI";
+    private static final Integer[] WHITES_BLACKS = new Integer[]{
+            0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0,
+            0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0};
+
     String TITLE = I18n.format("gui.bardmania.gui_keyboard.title");
 
     public GuiKeyboard()
@@ -21,6 +35,23 @@ public class GuiKeyboard extends GuiScreen
     @Override
     public void initGui()
     {
+        buttonList.clear();
+        for (int i = 0; i < KEYSCAN_VALUES.length/2; i++)
+        {
+            buttonList.add(new GuiKeyButton(KEYSCAN_VALUES[i],
+                    55 + (width / (13 * 25)) + i*25,
+                    125 - WHITES_BLACKS[i] * 25,
+                    20, 20,
+                    I18n.format("gui.bardmania.gui_key_" + (KEY_TOPS.subSequence(i, i+1).equals(",") ? "COMMA" : KEY_TOPS.subSequence(i, i+1)))));
+        }
+        for (int i = KEYSCAN_VALUES.length/2; i < KEYSCAN_VALUES.length; i++)
+        {
+            buttonList.add(new GuiKeyButton(KEYSCAN_VALUES[i],
+                    55 + (width / (13 * 25)) + (i-KEYSCAN_VALUES.length/2)*25,
+                    55 - WHITES_BLACKS[i] * 25,
+                    20, 20,
+                    I18n.format("gui.bardmania.gui_key_" + (KEY_TOPS.subSequence(i, i+1).equals(",") ? "COMMA" : KEY_TOPS.subSequence(i, i+1)))));
+        }
         super.initGui();
     }
 
@@ -49,6 +80,8 @@ public class GuiKeyboard extends GuiScreen
     @Override
     protected void actionPerformed(GuiButton button) throws IOException
     {
+        if (MidiHelper.hasKeyNoteMap(button.id))
+            sendNote(MidiHelper.getKeyNoteMap(button.id) + (isShiftKeyDown() ? 12 : 0));
         super.actionPerformed(button);
     }
 
