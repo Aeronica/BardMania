1325eebdbaf14762cbdccc59273b09cf0d3bbd15
Aeronica <pmboese@gmail.com>
2018-05-28 22:35:41 -0500

    Use a JSON model and a set of 16x16 textures for Melodic Toms instead of the ModelBase version and its 64x64 texture sheet.

--- src/main/java/net/aeronica/mods/bardmania/client/render/LayerMelodicToms.java
+++ src/main/java/net/aeronica/mods/bardmania/client/render/LayerMelodicToms.java
@@ -1,27 +1,23 @@
 package net.aeronica.mods.bardmania.client.render;
 
-import net.aeronica.mods.bardmania.Reference;
-import net.aeronica.mods.bardmania.client.model.ModelMelodicToms;
+import net.aeronica.mods.bardmania.BardMania;
 import net.aeronica.mods.bardmania.common.ModLogger;
 import net.aeronica.mods.bardmania.item.ItemHandHeld;
 import net.aeronica.mods.bardmania.object.HoldType;
 import net.aeronica.mods.bardmania.object.Instrument;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.entity.Render;
+import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
 import net.minecraft.client.renderer.entity.RenderPlayer;
 import net.minecraft.client.renderer.entity.layers.LayerRenderer;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.util.ResourceLocation;
+import net.minecraft.item.ItemStack;
 
 import java.util.Map;
 
 public class LayerMelodicToms implements LayerRenderer<EntityLivingBase>
 {
     private static final LayerMelodicToms LAYER_MELODIC_TOMS = new LayerMelodicToms();
-    private static final ModelMelodicToms modelMelodicToms = new ModelMelodicToms();
-    private static final ResourceLocation textureMelodicToms = new ResourceLocation(Reference.MOD_ID, "textures/model/melodic_toms.png");
     private static boolean isAdded = false;
 
     @Override
@@ -30,13 +26,14 @@ public class LayerMelodicToms implements LayerRenderer<EntityLivingBase>
         if (!entitylivingbaseIn.isInvisible() && (entitylivingbaseIn.getHeldItemMainhand().getItem() instanceof ItemHandHeld))
         {
             Instrument instrument = ((ItemHandHeld) entitylivingbaseIn.getHeldItemMainhand().getItem()).getInstrument();
-            if (instrument.general.holdType.equals(HoldType.TWO_HANDED_DRUM))
+            ItemStack itemStack = entitylivingbaseIn.getHeldItemMainhand();
+            if (instrument.general.holdType.equals(HoldType.TWO_HANDED_DRUM) && instrument.id.equalsIgnoreCase("melodic_toms"))
             {
-                Render<Entity> renderEntity = Minecraft.getMinecraft().getRenderManager().getEntityRenderObject(entitylivingbaseIn);
-                renderEntity.bindTexture(textureMelodicToms);
                 GlStateManager.pushMatrix();
-                modelMelodicToms.setRotationAngles(limbSwing, limbSwingAmount, partialTicks, netHeadYaw, headPitch, scale, entitylivingbaseIn);
-                modelMelodicToms.render(entitylivingbaseIn, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale);
+                GlStateManager.translate(0, 0.0625 * 9, -0.0625 * 9);
+                GlStateManager.rotate(180, 0, 1, 0);
+                GlStateManager.rotate(180, 1, 0, 0);
+                BardMania.proxy.getMinecraft().getRenderItem().renderItem(itemStack, entitylivingbaseIn, ItemCameraTransforms.TransformType.NONE, false);
                 GlStateManager.popMatrix();
             }
         }
