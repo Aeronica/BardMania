5f8525805ead6e988012cf114b77480f0ac530f5
Aeronica <pmboese@gmail.com>
2018-07-10 20:51:59 -0500

    Faster attack and longer decay on xylophone play movement.

--- src/main/java/net/aeronica/mods/bardmania/client/action/XylophoneTimelines.java
+++ src/main/java/net/aeronica/mods/bardmania/client/action/XylophoneTimelines.java
@@ -15,18 +15,18 @@ public class XylophoneTimelines
     {
         timeline.beginParallel()
                 .push(tweenEngine.to(modelDummy, LEFT_ARM_ACTION_ROT_X, 0.09f).target(-0.1F * leftHandNote(normalizedNote)).ease(TweenEquations.Sine_InOut))
-                .push(tweenEngine.to(modelDummy, LEFT_ARM_ACTION_ROT_Y, 0.15f).target(leftHandNotePosition(normalizedNote)).ease(TweenEquations.Sine_InOut))
+                .push(tweenEngine.to(modelDummy, LEFT_ARM_ACTION_ROT_Y, 0.10f).target(leftHandNotePosition(normalizedNote)).ease(TweenEquations.Sine_InOut))
                 .push(tweenEngine.to(modelDummy, RIGHT_ARM_ACTION_ROT_X, 0.09f).target(-0.1F * rightHandNote(normalizedNote)).ease(TweenEquations.Sine_InOut))
-                .push(tweenEngine.to(modelDummy, RIGHT_ARM_ACTION_ROT_Y, 0.15f).target(RightHandNotePosition(normalizedNote)).ease(TweenEquations.Sine_InOut))
+                .push(tweenEngine.to(modelDummy, RIGHT_ARM_ACTION_ROT_Y, 0.10f).target(RightHandNotePosition(normalizedNote)).ease(TweenEquations.Sine_InOut))
 
                 .push(tweenEngine.to(modelDummy, RIGHT_HAND_ITEM_ROT_Y, 0.15f).target(-10F * rightHandNote(normalizedNote)).ease(TweenEquations.Sine_InOut))
                 .push(tweenEngine.to(modelDummy, LEFT_HAND_ITEM_ROT_Y, 0.15f).target(10F * leftHandNote(normalizedNote)).ease(TweenEquations.Sine_InOut))
                 .end()
                 .beginParallel()
                 .push(tweenEngine.to(modelDummy, LEFT_ARM_ACTION_ROT_X, 0.09f).target(0f).ease(TweenEquations.Sine_InOut))
-                .push(tweenEngine.to(modelDummy, LEFT_ARM_ACTION_ROT_Y, 0.15f).target(0f).ease(TweenEquations.Sine_InOut))
+                .push(tweenEngine.to(modelDummy, LEFT_ARM_ACTION_ROT_Y, 0.19f).target(0f).ease(TweenEquations.Sine_InOut))
                 .push(tweenEngine.to(modelDummy, RIGHT_ARM_ACTION_ROT_X, 0.09f).target(0f).ease(TweenEquations.Sine_InOut))
-                .push(tweenEngine.to(modelDummy, RIGHT_ARM_ACTION_ROT_Y, 0.15f).target(0f).ease(TweenEquations.Sine_InOut))
+                .push(tweenEngine.to(modelDummy, RIGHT_ARM_ACTION_ROT_Y, 0.19f).target(0f).ease(TweenEquations.Sine_InOut))
 
                 .push(tweenEngine.to(modelDummy, RIGHT_HAND_ITEM_ROT_Y, 0.15f).target(0f).ease(TweenEquations.Sine_InOut))
                 .push(tweenEngine.to(modelDummy, LEFT_HAND_ITEM_ROT_Y, 0.15f).target(0f).ease(TweenEquations.Sine_InOut))
