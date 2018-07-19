daa3ae7027ebba2b6fd74609365405dc873ff3d2
Aeronica <pmboese@gmail.com>
2018-07-19 17:38:00 -0500

    Ensure the state is not optimized out of existence

--- src/main/java/net/aeronica/mods/bard_mania/server/caps/BardActionImpl.java
+++ src/main/java/net/aeronica/mods/bard_mania/server/caps/BardActionImpl.java
@@ -4,7 +4,7 @@ import net.aeronica.mods.bard_mania.client.action.ModelDummy;
 
 public class BardActionImpl implements IBardAction
 {
-    private boolean isInstEquipped = false;
+    private volatile boolean isInstEquipped = false;
     private boolean tempBoolean = false;
     private ModelDummy modelDummy = new ModelDummy();
 
